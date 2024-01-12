36535446e1c70354c89728ea3754389b7dfd1c02
shree <shree@LAPTOP-2QEA4LIA>
2024-01-12 16:27:09 +0530

    By saurabh

---
+++ Git_Demo7/src/com/cjc/demo/SS.java
@@ -0,0 +1,16 @@
+package com.cjc.demo;
+
+public class SS {
+
+	
+	public static void m1()
+	{
+		System.out.println("THIS IS M1");
+	}
+	
+	public static void main(String[] args) 
+	{
+		
+		SS.m1();
+	}
+}
