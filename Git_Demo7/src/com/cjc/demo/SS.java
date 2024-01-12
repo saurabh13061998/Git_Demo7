101da2f9165ca8048b0e2aaca6cf0324a0a57b42
shree <shree@LAPTOP-2QEA4LIA>
2024-01-12 16:32:10 +0530

    by saurabh

--- Git_Demo7/src/com/cjc/demo/SS.java
+++ Git_Demo7/src/com/cjc/demo/SS.java
@@ -7,16 +7,24 @@ public class SS {
 	{
 		System.out.println("THIS IS M1");
 	}
+	
 	public static void m2()
 	{
 		System.out.println("THIS IS M2");
+	}
+	
+	
+	public static void m3()
+	{
+		System.out.println("THIS IS M3");
 	}
 	
 	public static void main(String[] args) 
 	{
 		
 		SS.m1();
-	       SS.m2();
+	    SS.m2();
+	    SS.m3();
 
 	}
 }
